0c258e3c75adec92e0cfd72e7e9da29b950500ff
AuroraDoc <kenyonmg83@gmail.com>
2025-07-25 16:31:25 -0400

    Start to add the database functionality to store player data

--- newDnD/src/App.jsx
+++ newDnD/src/App.jsx
@@ -3,13 +3,25 @@ import { Route, Routes, } from "react-router-dom";
 import "./styles/App.css";
 import Home from "./pages/Home";
 import PlayerInventory from "./pages/PlayerInventory"
-import ItemsTable from "./components/ItemsTable";
-//const sqlite3 = require("sqlite3").verbose()
-
 import { mccoy, dennis, mak, bella } from "../players" 
+import pg from "pg";
+
+
+const db = new pg.Client({
+  user: process.env.REACT_APP_dbUser,
+  host: process.env.REACT_APP_dbHost,
+  database: process.env.REACT_APP_database,
+  password: process.env.REACT_APP_dbPassword,
+  port: process.env.REACT_APP_dbPort 
+
+})
+
+db.connect()
+
+let playerData = db.query("SELECT * FROM dndusers")
+
 
 let players = [mccoy, dennis, mak, bella]
-
 
 function App() {
 
@@ -18,7 +30,7 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Home players={players}/>} />
-
+          
           {players.map(player => <Route path={player.playerName} element={<PlayerInventory player={player}/>}/>)}
         </Routes>
       </main>
